37781dfbfdbf4fd0c9820b48880187396acf83aa
gtburchett <gburchett88@gmail.com>
2021-03-15 22:41:07 -0500

    Finished 2/3 of calculate_demographic_data function. Added .gitignore file and added my test notebook for solving these solutions.

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,30 +3,30 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df.race.value_counts
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df.age[df['sex'] == 'Male'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = round(df.education[df['education'] == 'Bachelors'].count() / df.education.count() * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df.education[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    lower_education = df.education[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round(higher_education[df['salary'] == '>50K'].count() / higher_education.count() * 100, 1)
+    lower_education_rich = round(lower_education[df['salary'] == '>50K'].count() / lower_education.count() * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = None
